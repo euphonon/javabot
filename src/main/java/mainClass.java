d75eae75e1cd93373bd2416f4d780e27b877ff68
gigitac <belnest.odupong@gmail.com>
2021-01-31 00:02:23 -0700

    set up API

--- src/main/java/mainClass.java
+++ src/main/java/mainClass.java
@@ -1,2 +1,8 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 public class mainClass {
+    public static void main(String[] args) {
+        DiscordApi API = new DiscordApiBuilder().setToken(System.getenv("TOKEN")).login().join();
+    }
 }
